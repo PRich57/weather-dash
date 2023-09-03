315ca3399b016d29e1baa4327b301674a7a5bb5a
PRich57 <pcrichards57@gmail.com>
2023-09-03 15:08:46 -0600

    Add local storage and history button functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,27 +47,80 @@ $(function () {
       currentTemp.text('Temp: ' + Math.round(data.main.temp) + "°F");
       // Update text node of current wind speed, rounded for appearance
       currentWind.text('Wind Speed: ' + Math.round(data.wind.speed) + " mph");
-      // Update 
+      // Update text node of current humidity, rounded for appearance
       currentHumidity.text('Humidity: ' + Math.round(data.main.humidity) + "%");
+      // Set src attribute with data icon code to load in the corresponding png
       icon.attr('src', './assets/images/' + data.weather[0].icon + '.png');
     });
   }
 
+  // Create function for fetching forecast data
   function getForecast(requestUrlArg) {
     fetch(requestUrlArg)
     .then(function (response) {
       return response.json();
     })
+    // Use json response in next then function
     .then(function (data) {
       console.log(data);
+      // for each card in the cards array
       for (var i = 0; i < cards.length; i++) {
+        // Update the date header for cards[i] in cards array
         cards[i].children().children('h4').text(today.add(i + 1, 'day').format('MMM D, YYYY'));
-        // cards[i].children('ul').children().eq(0).text('Temp: ' + )
+        // Update the temp for cards[i] in cards array
+        // cards[i].children('ul').children().eq(0).text('Temp: ' +  + '°F');
+        // Update wind speed for cards[i] in cards array
+        // cards[i].children('ul').children.eq(1).text('Wind Speed: ' +  + ' mph');
+        // Update humidity for cards[i] in cards array
+        // cards[i].children('ul').children.eq(2).text('Humidity: ' +  + '%');
         
-      }
+      }   // May need to average out the daily temp, wind speed, humidity
     })
   }
+
+  // Create function to store search history in local memory
+  function searchHistory() {
+    // Save query to local storage
+    var userSearch = $('input').val();
+    // Trim any potential whitespace from user's input
+    var searchItem = userSearch.trim();
+    
+    // Exit condition
+    if (searchItem === "") {
+      return;
+    }
+
+    // Get existing history from local storage
+    var storedHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    // Add new search to the existing search history
+    storedHistory.push(searchItem);
+    // Save the updated searchHistory back to local storage
+    localStorage.setItem("searchHistory", JSON.stringify(storedHistory));
+
+    // Clear the input field
+    $('input').val("");
+
+    // Call function to update the search history list
+    listHistory();    
+  }
+
+  // Create function to list the search history for the user to see and use
+  function listHistory() {
+    // Use the history reference variable and clear any existing list items
+    history.val("");
+
+    // Get items from local storage
+    var listHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+    // Create buttons for each stored item
+    for (const searchItem in listHistory) {
+      var historyBtn = $("<button>");
+      historyBtn.textContent = searchItem;
+      history.append(historyBtn);
+    }
+  }
   
+  // NEED TO MAKE INPUT TO GETCURRRENT AND GETFORECAST FUNCTIONS BE THE CITY NAME ONLY TO MAKE THE HISTORY BUTTONS WORK CORRECTLY
 
   
   // EVENT LISTENERS
@@ -84,8 +137,13 @@ $(function () {
     
     getCurrent(userCurrent);
     getForecast(userForecast);
-    $('input').val("");
+
   });
+
+  historyBtn.on("click", function (event) {
+    getCurrent()
+  })
+
 
   // FUNCTION CALLS FOR INITIAL CONTENT
   getCurrent(initialCurrent);
