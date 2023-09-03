555ef7b086186d82b2c6a179b611908ad9b3960e
PRich57 <pcrichards57@gmail.com>
2023-09-03 11:10:59 -0600

    Add js for the current weather section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,56 @@
-// VARIABLE DECLARATIONS
-// // var cityName = prompt("Which city would you like to view?");
-// // var cityName = document.getElementById("city-name").value;
-// var fetchBtn = document.getElementById('fetch-button');
-// var mainSection = document.querySelector("main");
-// var history = document.querySelector(".search-history");
+$(function () {
+  // VARIABLE DECLARATIONS
+  var fetchBtn = $("button");
+  var userCity;
+  var city = $(".city");
+  var currentTemp = $('.temp');
+  var currentWind = $('.wind');
+  var currentHumidity = $('.humidity');
+  var history = $(".search-history");
+  var apiKey = "&appid=9217e4926ac9dcff03755cdfef766696";
 
-var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9217e4926ac9dcff03755cdfef766696';
+  
 
-var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=9217e4926ac9dcff03755cdfef766696'
-
-var apiKey = "9217e4926ac9dcff03755cdfef766696"
-
-
-
-
-
-
-
-
-// // FUNCTIONS
-function getApi(requestUrlArg) {
-  fetch(requestUrlArg)
+  
+  
+  // // FUNCTIONS
+  function getApi(requestUrlArg) {
+    fetch(requestUrlArg)
     .then(function (response) {
       // console.log(response);
       return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-}
-// getApi(currentWeatherUrl);
+    })
+    .then(function (data) {
+      console.log(data);
+      city.text(data.name);
+      currentTemp.text('Temp: ' + Math.round(data.main.temp) + "°F");
+      currentWind.text('Wind Speed: ' + Math.round(data.wind.speed) + " mph");
+      currentHumidity.text('Humidity: ' + data.main.humidity + "%");
+    });
+  }
+  
+  
+  // function appendResults(userInfo) {
 
-// getApi(forecastUrl);
-
-
-
-
-
-
-// EVENT LISTENERS
-// fetchBtn.on("click", )
-
-
-
-
-
+    
+  // }
+  
+  
+  
+  // EVENT LISTENERS
+  fetchBtn.on("click", function (event) {
+    var userCity = $('input').val();
+    var currentWeatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + userCity + apiKey;
+    var forecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + userCity + apiKey;
+    
+    
+    console.log(userCity);
+    
+    getApi(currentWeatherUrl);
+    // appendResults(userCity);
+    // getApi(forecastUrl);
+  });
+  
+});
