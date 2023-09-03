765027b946ad1b743152786c6f6bf1c02f5646c2
PRich57 <pcrichards57@gmail.com>
2023-09-02 18:18:24 -0600

    Add styling with bootstrap and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
 // VARIABLE DECLARATIONS
-// var cityName = prompt("Which city would you like to view?");
-// var cityName = document.getElementById("city-name").value;
-var fetchBtn = document.getElementById('fetch-button');
-var mainSection = document.querySelector("main");
-var history = document.querySelector(".search-history");
+// // var cityName = prompt("Which city would you like to view?");
+// // var cityName = document.getElementById("city-name").value;
+// var fetchBtn = document.getElementById('fetch-button');
+// var mainSection = document.querySelector("main");
+// var history = document.querySelector(".search-history");
 
-var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9217e4926ac9dcff03755cdfef766696';
+// var currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9217e4926ac9dcff03755cdfef766696';
 
-var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=9217e4926ac9dcff03755cdfef766696'
-
-
+// var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=9217e4926ac9dcff03755cdfef766696'
 
 
 
@@ -18,24 +16,26 @@ var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=denver&app
 
 
 
-// FUNCTIONS
-function getApi(requestUrlArg) {
-  fetch(requestUrlArg)
-    .then(function (response) {
-      // console.log(response);
-      return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    var city = document.createElement("h2");
-    var weather = document.createElement("p");
-    city.textContent = data.name;
-    weather.textContent = data.weather[0].description;
-    mainSection.appendChild(city);
-    mainSection.appendChild(weather);
-  })
-}
-getApi(currentWeatherUrl);
+
+
+// // FUNCTIONS
+// function getApi(requestUrlArg) {
+//   fetch(requestUrlArg)
+//     .then(function (response) {
+//       // console.log(response);
+//       return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     var city = document.createElement("h2");
+//     var weather = document.createElement("p");
+//     city.textContent = data.name;
+//     weather.textContent = data.weather[0].description;
+//     mainSection.appendChild(city);
+//     mainSection.appendChild(weather);
+//   })
+// }
+// getApi(currentWeatherUrl);
 
 // getApi(forecastUrl);
 
